0b9cb7fa2d5442fa58b81540d218e36ec4c09893
Nick Gerakines <nick.gerakines@gmail.com>
2023-04-13 13:51:56 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,72 @@
 use anyhow::{Result, Error};
+use serde_json_path::JsonPath;
 use std::{process::ExitCode, env, fs::File, io::BufReader};
 
 #[tokio::main]
 async fn main() -> Result<ExitCode> {
 
-    println!("GITHUB_HEAD_REF {}", env::var("GITHUB_HEAD_REF").unwrap_or_default());
-    println!("GITHUB_REPOSITORY {}", env::var("GITHUB_REPOSITORY").unwrap_or_default());
-    println!("GITHUB_REF_TYPE {}", env::var("GITHUB_REF_TYPE").unwrap_or_default());
-    println!("GITHUB_REPOSITORY_OWNER {}", env::var("GITHUB_REPOSITORY_OWNER").unwrap_or_default());
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <key_prefixes>", args[0]);
+        return Ok(ExitCode::FAILURE);
+    }
+
+    let key_prefixes: Vec<&str> = args[1].split(',').collect();
 
     let event: serde_json::Value = {
         let event_path = env::var("GITHUB_EVENT_PATH").map_err(|err| Error::msg(err).context("GITHUB_EVENT_PATH not set"))?;
-        let file = File::open(event_path)?;
+        let file = File::open(event_path).map_err(|err| Error::msg(err).context("GITHUB_EVENT_PATH not set"))?;
         let reader = BufReader::new(file);
         serde_json::from_reader(reader)
     }?;
 
-    println!("{:?}", event);
+    let number_path = JsonPath::parse("$.number")?;
+    let number_value = number_path.query(&event).at_most_one()?;
 
-    // serde_json::from_str("event_path")?;
+    if number_value.is_none() {
+        println!("PR number not found.");
+        return Ok(ExitCode::FAILURE);
+    }
 
-    // let key_prefixes: Vec<&str> = cli.key_prefixes.split(',').collect();
+    let title_path = JsonPath::parse("$.pull_request.title")?;
+    let search_title = title_path.query(&event).at_most_one()?.map(|v| v.as_str().unwrap_or_default()).unwrap_or_default();
 
-    // let (owner, repository) = match cli.repository.contains("/") {
-    //     true => {
-    //         let mut split = cli.repository.split('/');
-    //         Ok((split.next().unwrap(), split.next().unwrap()))
-    //     },
-    //     _ => Err(anyhow!("Invalid repository name")),
-    // }?;
+    if search_title.is_empty() {
+        println!("Title is empty");
+        return Ok(ExitCode::FAILURE);
+    }
 
+    let body_path = JsonPath::parse("$.pull_request.body")?;
+    let search_body = body_path.query(&event).at_most_one()?.map(|v| v.as_str().unwrap_or_default()).unwrap_or_default();
 
-    // let client = match cli.github_token {
-    //     Some(value) => octocrab::Octocrab::builder()
-    //         .personal_token(value.clone())
-    //         .build()?,
-    //     _ => octocrab::Octocrab::builder().build()?,
-    // };
+    if search_body.is_empty() {
+        println!("Body is empty");
+        return Ok(ExitCode::FAILURE);
+    }
 
-    // let pr = client.pulls(owner, repository).get(cli.pr_num).await;
+    let mut title_found = false;
+    let mut body_found = false;
+    for prefix in key_prefixes.clone() {
+        if !title_found && search_title.contains(prefix) {
+            title_found = true;
+        }
+        if !body_found && search_body.contains(prefix) {
+            body_found = true;
+        }
+        if title_found && body_found {
+            break;
+        }
+    }
 
-    // if let Err(err) = pr {
-    //     println!("Error: {}", err);
-    //     return Ok(ExitCode::FAILURE);
-    // }
-    // let pr = pr.unwrap();
+    if !title_found {
+        println!("Title does not contain any key prefixes");
+        return Ok(ExitCode::FAILURE);
+    }
 
-    // let search_title = pr.title.unwrap_or_default();
-    // let search_body = pr.body.unwrap_or_default();
-
-    // let mut title_found = cli.include_title;
-    // let mut body_found = cli.include_body;
-    // for prefix in key_prefixes.clone() {
-    //     if !title_found && search_title.contains(prefix) {
-    //         title_found = true;
-    //     }
-    //     if !body_found && search_body.contains(prefix) {
-    //         body_found = true;
-    //     }
-    //     if title_found && body_found {
-    //         break;
-    //     }
-    // }
-
-    // if !title_found {
-    //     println!("Title does not contain any key prefixes");
-    //     return Ok(ExitCode::FAILURE);
-    // }
-
-    // if !body_found {
-    //     println!("Body does not contain any key prefixes");
-    //     return Ok(ExitCode::FAILURE);
-    // }
+    if !body_found {
+        println!("Body does not contain any key prefixes");
+        return Ok(ExitCode::FAILURE);
+    }
 
     Ok(ExitCode::SUCCESS)
 }
